ed2bc7dc97256480653f8a0e548d8abfd4d0bef6
nabilkencana <nabilkencana20@gmail.com>
2026-07-10 02:04:41 +0700

    fix: force ipv4 address resolution to prevent connection delays in node environment

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dns from 'node:dns';
+dns.setDefaultResultOrder('ipv4first');
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
